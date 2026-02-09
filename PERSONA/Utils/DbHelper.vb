796edd8d9b8c9741c7462fbfab4ab3bcf86437f1
Dylan Rodríguez Bogantes <drodriguezb@edu.uc.ac.cr>
2026-02-09 17:18:05 -0600

    Añade clase DbHelper para gestión de base de datos
    Se incorpora el archivo DbHelper.vb al proyecto, incluyendo la clase DbHelper con métodos para conectar a SQL Server y crear registros de personas en la base de datos. Se actualiza el proyecto para incluir el nuevo archivo. Final clase 3

---
+++ PERSONA/Utils/DbHelper.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.SqlClient
+Imports System.Web.WebSockets
+
+Public Class DbHelper
+   
+    Private ReadOnly connectionString As String = ConfigurationManager.ConnectionStrings("II-46ConnectionString").ConnectionString
+
+    Public Function Getconexion() As SqlConnection
+        Return New SqlConnection(connectionString)
+    End Function
+
+    Public Function CrearPersona() As Integer
+        Dim Sql As String = "INSERT INTO Personas (Nombre, Apellidos, TipoDocumento, NumeroDocumento, FechaNacimiento, Correo)
+        VALUES (@Nombre, @Apellidos, @TipoDocumento, @NumeroDocumento, @FechaNacimiento, @Correo)"
+
+        'CONEXION A LA BASE DE DATOS
+        Using conn As SqlConnection = Getconexion()
+            Using cmd As New SqlCommand(Sql, conn)
+                conn.Open()
+                Return cmd.ExecuteNonQuery()
+            End Using
+        End Using
+        Return 0
+    End Function
+
+
+
+
+End Class
